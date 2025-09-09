1a2d4098afc70d31a0297ec72357ee67eb645752
EngageAutomations Deploy <deploy@engageautomations.com>
2025-09-08 22:05:39 -0400

    Remove rate limiting import to fix deployment errors

--- oauth_server.js
+++ oauth_server.js
@@ -33,7 +33,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
+// const rateLimit = require('express-rate-limit'); // Temporarily disabled
 const { Pool } = require('pg');
 const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
